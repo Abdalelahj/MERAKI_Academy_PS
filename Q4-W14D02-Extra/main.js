3cd554f9ac4bbafbb674e04a99c28fb24b6f0fa9
Abdalelahj <abdalelahaljamal@gmail.com>
2024-10-11 00:08:05 +0300

    sol

--- Q4-W14D02-Extra/main.js
+++ Q4-W14D02-Extra/main.js
@@ -6,6 +6,19 @@ write a function to return the largest word in the string
 
 const longestWord = (str) => {
   // YOUR CODE HERE
+  const a = str.split(" ");
+  // console.log(str.split(" "));
+  const f = a.reduce((acc, item, i) => {
+    if (a[i + 1]) {
+      acc=a[0]
+      
+      if (acc.length < a[i + 1].length) {
+          acc= a[i + 1]
+      }
+    }    
+    return acc;
+  }, 0);
+  return f;
 };
 
 /* 
--- Q5-W14D03-Main/main.js
+++ Q5-W14D03-Main/main.js
@@ -6,13 +6,25 @@ Have the function findIntersection(strArr) read the array of strings stored in s
 
 const findIntersection = (strArr) => {
   // YOUR CODE HERE
+  let first=strArr.slice(0,1)
+  let sec= strArr.slice(1)
+  let common=[];
+  
+  sec.join().split(",").map((item)=>{
+     first.join().split(",").forEach((element) => {
+    if(element===item){
+      common.push(item)
+        }
+    }); 
+  })  
+  return common.length?common.join():false 
 };
 
 /*
 Examples:
-findIntersection(['1, 3, 4, 7, 13', '1, 2, 4, 13, 15']); // => '1,4,13'
-findIntersection(['1, 3, 9, 10, 17, 18', '1, 4, 9, 10']); // => '1,9,10'
-findIntersection(['2, 5, 7, 14', '1, 4, 13, 15, 25']); // => false
+findIntersection(['1, 3, 4, 7, 13' , '1, 2, 4, 13, 15']); // => '1,4,13'
+findIntersection(['1, 3, 9, 10, 17, 18' , '1, 4, 9, 10']); // => '1,9,10'
+findIntersection(['2, 5, 7, 14' , '1, 4, 13, 15, 25']); // => false
 findIntersection(['', '1, 2, 4, 13, 15']); // => false
 */
 module.exports = { findIntersection };
