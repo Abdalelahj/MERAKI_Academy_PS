aa3371b93e54d7188bb2f73b14baeb617db468fc
Abdalelahj <abdalelahaljamal@gmail.com>
2024-11-18 04:31:13 +0300

    extra sol

--- Q20-W20D01-Extra/main.js
+++ Q20-W20D01-Extra/main.js
@@ -6,11 +6,20 @@ Write a function accepts an array of integers and returns the Maximum product po
 
 const maximumProductOfThree = (arr) => {
   // YOUR CODE HERE
+  // console.log(Math.max(...arr));
+  let first =Math.max(...arr)
+   arr.splice(arr.indexOf(first),1)
+   let sec=Math.max(...arr)
+   arr.splice(arr.indexOf(sec),1)
+   let third=Math.max(...arr)
+return first*sec*third   
 };
+
 
 /*
 Examples:
 maximumProductOfThree([3, 1, 3, 7]); // => 63
+maximumProductOfThree([0, 2, 3]); // => 0
 maximumProductOfThree([0, 2, 3]); // => 0
 maximumProductOfThree([2, 3, 5]); // => 30
 maximumProductOfThree([10, 4, 5]); // => 200
