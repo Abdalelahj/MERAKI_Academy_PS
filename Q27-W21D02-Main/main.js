12c6a097da1f2937677f2750d0940c967eb3fe39
Abdalelahj <abdalelahaljamal@gmail.com>
2024-11-27 00:45:25 +0300

    main sol

--- Q27-W21D02-Main/main.js
+++ Q27-W21D02-Main/main.js
@@ -7,11 +7,29 @@ If the url does not contain a query string, return undefined
 
 const getQueryString = (url) => {
   // YOUR CODE HERE
+  let selected = [];
+
+  if (url.includes("?")) {
+    const reg = /\b%\d+/g;
+    url = url.replace(reg, " ");
+    const newArr = url.split("?")[1]?.split("&");
+    for (let index = 0; index < newArr.length; index++) {
+      let dest = newArr[index].split("=");
+      selected.push(dest);
+    }
+    return selected;
+  } else {
+    return undefined;
+  }
 };
+
+// console.log(getQueryString("http://example.com?a=lol")); //=> [ [ "a", "lol" ] ]
+// console.log(getQueryString("http://example.com?a=lol&b=88"));
+// console.log(getQueryString("http://example.com?msg=lol%20world"));
+// console.log(getQueryString("http://example.com"));
 
 /* 
 Examples:
-getQueryString("http://example.com?a=lol"); //=> [ [ "a", "lol" ] ]
 getQueryString("http://example.com?a=lol&b=88"); //=> [ [ "a", "lol" ], [ "b", "88" ] ]
 getQueryString("http://example.com?msg=lol%20world"); //=> [ [ "msg", "lol world" ] ]
 getQueryString("http://example.com"); //=> undefined
