44a6d8844854f129a5af5d72a5f1b03213a4cd06
Abdalelahj <abdalelahaljamal@gmail.com>
2025-01-21 19:43:31 +0300

    new problem

---
+++ more/Q33/main.js
@@ -0,0 +1,30 @@
+/* 
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+*/
+
+const isPalindrome = function (x) {
+    const newX = x.toString()
+    // let condition = false
+    // for (let i = 0; i < newX.length; i++) {
+    //     if (newX[i] == newX[newX.length - 1 - i]) {
+    //         condition = true;
+    //     } else {
+
+    //         condition = false
+    //     }
+    // }
+    // return condition
+
+    //! better solution
+
+    return newX==newX.split('').reverse().join("")
+
+
+};
+
+
+console.log(isPalindrome(121));
+console.log(12%10);
